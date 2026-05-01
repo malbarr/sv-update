30cfc5aef2aa64e063442ec83040f04328fe7438
malbarr <malabr@iau.edu.sa>
2026-05-01 20:45:37 +0300

    fix: bump SW cache to v3 to force update on all devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sv-update-v2';
+const CACHE = 'sv-update-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
