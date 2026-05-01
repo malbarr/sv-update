656167e950f174ca58209ce6cdba88a7f5bf75f0
malbarr <malabr@iau.edu.sa>
2026-05-01 21:04:07 +0300

    feat: remove bottom nav, section cards now open full section screens with reference + articles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sv-update-v3';
+const CACHE = 'sv-update-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
